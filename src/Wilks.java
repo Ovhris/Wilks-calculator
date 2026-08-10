329e7cd25af5cbc3a84a799e9893ce89d1a68744
Ovhris <devid8225@gmail.com>
2026-08-10 23:14:25 +0300

    10.08.2026 введена переменная liftedWeight - поднятый вес. исправлена формула и новые коэффициенты

--- src/Wilks.java
+++ src/Wilks.java
@@ -3,85 +3,190 @@ import java.util.Scanner;
 
 public class Wilks {
 
+
     // Коэффициенты для мужчин
-    static final double a = -216.0475144;
-    static final double b = 16.2606339;
-    static final double c = -0.002388645;
-    static final double d = -0.00113732;
-    static final double e = 7.01863e-6;
-    static final double f = -1.291e-8;
+
+    static final double a = 47.46178854;
+
+    static final double b = 8.472061379;
+
+    static final double c = 0.07369410346;
+
+    static final double d = -0.001395833811;
+
+    static final double e =  7.07665973070743E-06;
+
+    static final double f =  -1.20804336482315E-08;
+
 
     // Коэффициенты для женщин
-    static final double a1 = 594.31747775582;
-    static final double b1 = -27.23842536447;
-    static final double c1 = 0.82112226871;
-    static final double d1 = -0.00930733913;
-    static final double e1 = 4.731582e-5;
-    static final double f1 = -9.054e-8;
+
+    static final double a1 =  -125.4255398;
+
+    static final double b1 = 13.71219419;
+
+    static final double c1 =  -0.03307250631;
+
+    static final double d1 = -0.001050400051;
+
+    static final double e1 =  9.38773881462799E-06;
+
+    static final double f1 = -2.3334613884954E-08;
+
 
     static char sex;
+
     static double sporsmenWeight;
 
+    static double liftedWeight;
+
+
     static double coefficientWilksMen;
+
     static double coefficientWilksWomen;
 
+    static double wilcsScore;
+
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
+
         // Ввод и проверка пола
+
         while (true) {
+
             System.out.print("Введите пол спортсмена (M/W): ");
+
             String sexInput = scanner.next();
 
+
             if (sexInput.length() == 1 && (sexInput.charAt(0) == 'M' || sexInput.charAt(0) == 'W')) {
+
                 sex = sexInput.charAt(0);
+
                 break;
+
             } else {
+
                 System.out.println("Ошибка: введите 'M' для мужчин или 'W' для женщин.");
+
             }
+
         }
 
-        // Ввод и проверка веса
+
+        // Ввод и проверка веса спортсмена
+
         while (true) {
+
             System.out.print("Введите вес спортсмена: ");
+
             String weightInput = scanner.next();
 
+
             try {
+
                 double weightValue = Double.parseDouble(weightInput);
 
+
                 if (weightValue <= 0) {
+
                     System.out.println("Ошибка: вес должен быть положительным числом больше нуля.");
+
                 } else {
+
                     sporsmenWeight = weightValue;
+
                     break;
+
                 }
+
             } catch (NumberFormatException ex) {
+
                 System.out.println("Ошибка: введите корректное число (например, 82.5).");
+
             }
+
         }
+
+
+        // Ввод и проверка поднятого веса
+
+        while (true) {
+
+            System.out.print("Введите поднятый вес (сумму): ");
+
+            String liftedInput = scanner.next();
+
+
+            try {
+
+                double liftedValue = Double.parseDouble(liftedInput);
+
+
+                if (liftedValue <= 0) {
+
+                    System.out.println("Ошибка: поднятый вес должен быть положительным числом больше нуля.");
+
+                } else {
+
+                    liftedWeight = liftedValue;
+
+                    break;
+
+                }
+
+            } catch (NumberFormatException ex) {
+
+                System.out.println("Ошибка: введите корректное число (например, 250.5).");
+
+            }
+
+        }
+
 
         // Расчёт коэффициента в зависимости от пола
+
         if (sex == 'M') {
+
             double x = sporsmenWeight;
+
             double denominator = a + b * x + c * Math.pow(x, 2) + d * Math.pow(x, 3)
+
                     + e * Math.pow(x, 4) + f * Math.pow(x, 5);
-            coefficientWilksMen = 500 / denominator;
+
+            coefficientWilksMen = 600 / denominator;
+
+            wilcsScore = liftedWeight * coefficientWilksMen;
+
 
             System.out.println("Коэффициент вилкса=" + coefficientWilksMen);
+
         } else {
+
             double x = sporsmenWeight;
+
             double denominator = a1 + b1 * x + c1 * Math.pow(x, 2) + d1 * Math.pow(x, 3)
+
                     + e1 * Math.pow(x, 4) + f1 * Math.pow(x, 5);
-            coefficientWilksWomen = 500 / denominator;
+
+            coefficientWilksWomen = 600 / denominator;
+
+            wilcsScore = liftedWeight * coefficientWilksWomen;
+
 
             System.out.println("Коэффициент вилкса=" + coefficientWilksWomen);
+
         }
 
+
+        System.out.println("Очки Уилкса=" + wilcsScore);
+
+
         scanner.close();
+
     }
+
 }
-
-
-
-
-
